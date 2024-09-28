08b0a1906dd3c3062037f850e4756213ccbf10ac
Kedar <khippalge@gmail.com>
2024-09-28 18:32:57 +0530

    first commit

---
+++ app/src/main/java/com/example/mitcarpooling/Ride.java
@@ -0,0 +1,15 @@
+package com.example.mitcarpooling;
+
+public class Ride {
+    public String getPublisherId() {
+        return "";
+    }
+
+    public Object getRideId() {
+        return null;
+    }
+
+    public String getPublisherEmail() {
+        return "";
+    }
+}
